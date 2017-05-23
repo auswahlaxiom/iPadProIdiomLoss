217520ea658204035e7e7137fd0251137388c017
Ada Turner <ada.l.turner@gmail.com>
2017-05-23 16:57:03 -0600

    setup main view controller

--- iPadProTraitCollectionIdiomLoss/MainViewController.swift
+++ iPadProTraitCollectionIdiomLoss/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  iPadProTraitCollectionIdiomLoss
 //
 //  Created by Ada Turner on 5/23/17.
@@ -8,18 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        title = "iPad Pro Idiom Loss"
+        view.backgroundColor = .magenta
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
